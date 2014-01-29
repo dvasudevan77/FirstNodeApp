8d38f233a0d3a5091f2d8c684727a419f37a4fd9
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-29 15:50:08 -0800

    Changed message

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,9 +1,7 @@
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  res.end('How are you?\n');
-  res.end('I am doing good.\n');
-  res.end('Weather is great outside.\n');
+  res.end('Hello World. How are you?\n');
 }).listen(1389, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1389/');
