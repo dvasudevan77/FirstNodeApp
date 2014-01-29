77bb54507c2c796ff37e62064c42174dea31fd51
Deepa Vasudevan <deepa.vasudevan@activenetwork.com>
2014-01-29 11:10:37 -0800

    Added more messages

--- hello-world-server.js
+++ hello-world-server.js
@@ -4,5 +4,6 @@ http.createServer(function (req, res) {
   res.end('Hello World\n');
   res.end('How are you?\n');
   res.end('I am doing good.\n');
-}).listen(1339, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1339/');
+  res.end('Weather is great outside.\n');
+}).listen(1389, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1389/');
